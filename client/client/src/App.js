0ce591911430227d08b5dcdc11e2b439d11891d7
KELFIDEL1337 <fideliskelechi10@gmail.com>
2026-09-22 11:57:20 +0100

    Update App.js

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,1 +1,13 @@
-Add first Project Arena home screen
+function App() {
+  return (
+    <div>
+      <h1>PROJECT ARENA</h1>
+      <button>PLAY</button>
+      <button>MATCHES</button>
+      <button>PROFILE</button>
+      <button>SETTINGS</button>
+    </div>
+  );
+}
+
+export default App;Add first Project Arena home screen
